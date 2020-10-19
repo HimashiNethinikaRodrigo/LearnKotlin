83820fbe0319ca73595e8f6cab7c5b4568731514
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-10-19 16:41:35 +0200

    add main function and main features in Kotlin as comment in main file

---
+++ src/Hello.kt
@@ -0,0 +1,13 @@
+/*
+Kotlin -
+general purpose language
+supports both object oriented programming paradigms and functional programming
+Safe, interoperability with java(support all java frameworks), tool friendly, concise
+Android, Server side development
+Kotlin/JVM, Kotlin/JS, Kotlin/Native
+ */
+
+fun main(args: Array<String>) {
+    println("Hello, world!")
+    println("It's a sunny and warm day!")
+}
