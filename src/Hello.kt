271b9d0c6bc80ffd3806b00f9d51fd52c414b658
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-10-19 21:45:25 +0200

    add comments to the code

--- src/Hello.kt
+++ src/Hello.kt
@@ -5,6 +5,11 @@ supports both object oriented programming paradigms and functional programming
 Safe, interoperability with java(support all java frameworks), tool friendly, concise
 Android, Server side development
 Kotlin/JVM, Kotlin/JS, Kotlin/Native
+
+Kotlin Compiled to Java byte code
+.java ---------- .class
+.kt   ---------- .class
+
  */
 
 fun main(args: Array<String>) {
