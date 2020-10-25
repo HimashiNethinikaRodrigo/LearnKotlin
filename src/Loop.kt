2d5c39fb8fab66b7cc702764e3765f1720c1440d
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-10-25 12:20:27 +0100

    in expression for define belonging and not belonging  defined in Kotlin

--- src/Loop.kt
+++ src/Loop.kt
@@ -40,23 +40,35 @@ fun forLoopIterating() {
     for (i in 1..9) {
         print(i) // 123456789
     }
+    println()
 
     //iterating over range - exclude upper bound include lower bound
     for (i in 1 until 9) {
         print(i) // 12345678
     }
+    println()
 
     //iterating over range with step including upper and lower bound
     for (i in 9 downTo 1 step 2) {
         print(i) // 97531
     }
+    println()
 
     for (ch in "abc") {
         print(ch + 1) //bcd
     }
+    println()
 }
+
+// belonging
+fun isLetter (c:Char) = c in 'a'..'z' || c in 'A'..'Z'
+
+// not belonging
+fun isNotDigit (c:Char) = c !in '0'..'9'
 
 fun main(){
     forLoopIterating()
+    println(isLetter('*'))
+    println(isNotDigit('x'))
 }
 
